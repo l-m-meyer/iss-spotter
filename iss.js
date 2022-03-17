7317692cb460354baa8655d6c121781abf1ccb7d
Lisa Meyer <lisamermey@gmail.com>
2022-03-17 17:48:58 -0400

    successfully fetch coordinates by IP using an API

--- iss.js
+++ iss.js
@@ -30,4 +30,23 @@ const fetchMyIP = (callback) => {
   });
 };
 
-module.exports = { fetchMyIP };
+const fetchCoordsByIP = (ip, callback) => {
+  request(`https://freegeoip.app/json/${ip}`, (error, response, body) => {
+    if (error) {
+      callback(error , null);
+      return;
+    }
+
+    //if non-200 status, assume server error
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+    
+    const { latitude, longitude } = JSON.parse(body);
+    callback(null, { latitude, longitude });
+  });
+};
+
+module.exports = { fetchMyIP, fetchCoordsByIP };
